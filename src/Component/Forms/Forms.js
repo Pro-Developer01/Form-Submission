d4238def77dfac648678b759bd0bc1abc63c28aa
Pro-Developer01 <yadavshashank70@gmail.com>
2022-08-22 01:03:59 +0530

    css file linked

--- src/Component/Forms/Forms.js
+++ src/Component/Forms/Forms.js
@@ -1,6 +1,7 @@
 import { useFormik } from 'formik'
 import React, { useState } from 'react'
 // import { date } from 'yup/lib/locale'
+import "./Forms.css"
 
 export default function Forms() {
     const [records, setRecords] = useState([])
@@ -17,32 +18,44 @@ export default function Forms() {
             setRecords([...records, newrecord]);
         }
     })
-    console.log(records);
     return (
         records &&
-        <div className="FormFields">
+        <div className="containerForms">
+            <div className="formFeilds">
+                
             <form onSubmit={formik.handleSubmit}>
-                <div className="row1">
-                    <input type="text" placeholder='Enter Name' name='name' value={formik.values.name} onChange={formik.handleChange} />
-                </div>
-                <div className="row1">
+                <div className="row" id="Personal Details" >
+                    <h3><u>Personal Details</u></h3>
+                    <label htmlFor="name">Name</label>
+                    <input type="text" placeholder='Enter Name' name='name' id='name' value={formik.values.name} onChange={formik.handleChange} />
+                    <label htmlFor="DOB">Date Of Birth or Age</label>
                     <input type="text" placeholder='DD/MM/YYYY 0r Age In Years' name='DOB' value={formik.values.DOB} onChange={formik.handleChange} />
-
-                    <select name="sex" className='Sex' placeholder='Enter Sex' value={formik.values.sex} onChange={formik.handleChange}>
+                    <label htmlFor="sex">Sex</label>
+                    <select name="sex" className='Sex' id="sex"placeholder='Enter Sex' value={formik.values.sex} onChange={formik.handleChange}>
                         <option value="Male">Male</option>
                         <option value="Female">Female</option>
                         <option value="Others">Others</option>
                     </select>
+                    <label htmlFor="mobile">Mobile</label>
+                    <input type="number" placeholder='Enter Mobile' name='mobile' id='mobile' value={formik.values.mobile} onChange={formik.handleChange} />
 
-                </div>
-                <div className="row1">
-                    <input type="text" placeholder='Enter Mobile' name='mobile' value={formik.values.mobile} onChange={formik.handleChange} />
+                    <label htmlFor="GovtId">Govt Issued ID</label>
+                    <select name="IDtype" id="IDType" placeholder='ID Type' value={formik.values.IDtype} onChange={formik.handleChange}>
+                        <option value="Aadhar">Aadhar</option>
+                        <option value="PAN">PAN</option>
+                        <option value="Driving Licence">Driving Licence</option>
+                    </select>
+
                     <input type="number" placeholder='Enter Govt ID' name='GovtID' value={formik.values.GovtID} onChange={formik.handleChange} />
+                </div>
+               
+                <div className="row1">
                     <button type='submit'>Submit</button>
                 </div>
 
             </form>
-            <div className="table">
+            </div>
+            <div className="tableFeilds">
                 <table >
                     <thead>
 
