a408762b20c7360d98a15b79e6816e781cf750d5
Pro-Developer01 <yadavshashank70@gmail.com>
2022-08-22 01:50:37 +0530

    styling update in personal details

--- src/Component/Forms/Forms.js
+++ src/Component/Forms/Forms.js
@@ -26,18 +26,39 @@ export default function Forms() {
             <form onSubmit={formik.handleSubmit}>
                 <div className="row" id="Personal Details" >
                     <h3><u>Personal Details</u></h3>
+                    <div className='InputFeilds' >
+                    
+                    
+                    <div >
+
                     <label htmlFor="name">Name</label>
                     <input type="text" placeholder='Enter Name' name='name' id='name' value={formik.values.name} onChange={formik.handleChange} />
+                    </div>
+                    
+                    <div >
+
                     <label htmlFor="DOB">Date Of Birth or Age</label>
                     <input type="text" placeholder='DD/MM/YYYY 0r Age In Years' name='DOB' value={formik.values.DOB} onChange={formik.handleChange} />
+                    </div>
+                    
+                    <div >
+
                     <label htmlFor="sex">Sex</label>
                     <select name="sex" className='Sex' id="sex"placeholder='Enter Sex' value={formik.values.sex} onChange={formik.handleChange}>
                         <option value="Male">Male</option>
                         <option value="Female">Female</option>
                         <option value="Others">Others</option>
                     </select>
+                    </div>
+                    
+                    <div >
+
                     <label htmlFor="mobile">Mobile</label>
                     <input type="number" placeholder='Enter Mobile' name='mobile' id='mobile' value={formik.values.mobile} onChange={formik.handleChange} />
+                    </div>
+
+                    
+                    <div >
 
                     <label htmlFor="GovtId">Govt Issued ID</label>
                     <select name="IDtype" id="IDType" placeholder='ID Type' value={formik.values.IDtype} onChange={formik.handleChange}>
@@ -47,6 +68,8 @@ export default function Forms() {
                     </select>
 
                     <input type="number" placeholder='Enter Govt ID' name='GovtID' value={formik.values.GovtID} onChange={formik.handleChange} />
+                    </div>
+                    </div>
                 </div>
                
                 <div className="row1">
